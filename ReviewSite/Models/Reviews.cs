33fb986f7b15ac7c9a9578ef787b40bbdf1562e6
Stephen2Page <stephen2page@gmail.com>
2016-10-07 10:18:06 -0400

    intial commit, models created
    Created models for Reviews and Categories

---
+++ ReviewSite/Models/Reviews.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSite.Models
+{
+    public class Reviews
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Rating { get; set; }
+        public DateTime VisitDate { get; set; }
+        public string Content { get; set; }
+        public DateTime ReviewDate { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+    }
+}

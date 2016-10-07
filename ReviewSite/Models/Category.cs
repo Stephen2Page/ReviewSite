656f657a83f4b1af5b0818bda3ef9af00393aea4
Stephen2Page <stephen2page@gmail.com>
2016-10-07 11:25:45 -0400

    Added formating to Index/Home page

--- ReviewSite/Models/Category.cs
+++ ReviewSite/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,9 @@ namespace ReviewSite.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+        [Display(Name = "Category")]
+        public string Name { get; set; }
+        public virtual ICollection<Reviews> Review { get; set; }
     }
 }
--- ReviewSite/Models/Reviews.cs
+++ ReviewSite/Models/Reviews.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -11,8 +13,10 @@ namespace ReviewSite.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public int Rating { get; set; }
+        [Display(Name = "Date of Visit")]
         public DateTime VisitDate { get; set; }
         public string Content { get; set; }
+        [Display(Name = "Date of Review")]
         public DateTime ReviewDate { get; set; }
 
         [ForeignKey("Category")]
